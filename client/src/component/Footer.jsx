679c9d4a7c3e5cfb383fead9aca4f6918aa4d24a
Loushik LK <loushikkumargiri@gmail.com>
2021-08-16 01:09:20 +0530

    db module update

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -11,8 +11,6 @@ const Footer = () => {
       <footer>
         <small className="text-light">Copyright {date} &copy; Made With &#x1F496; BY LK </small>
 
-        <NavLink exact className="justify-content-end" to="/admin/login"> <img className="logo" src="https://ordinarytravellers.herokuapp.com/image/admin/adminicon.png" alt="Admin" /></NavLink>
-
 
       </footer>
     </div>
